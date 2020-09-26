fa0e8e2787b4d281aa8df4f13031b77e5bceb85e
Anderson Santo <kalangoti@gmail.com>
2020-09-26 10:22:12 -0300

    created the project and the repository

---
+++ web/src/test/java/br/com/tcciakalah/TccIaKalahApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcciakalah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccIaKalahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
